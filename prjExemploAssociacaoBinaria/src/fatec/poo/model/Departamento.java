dabc499d0af9d18952c8c96c2a64011ec4a63b09
Pasquale <pasqualenetto@yahoo.com.br>
2025-03-31 23:29:09 -0300

    associacao binaria alterada

--- prjExemploAssociacaoBinaria/src/fatec/poo/model/Departamento.java
+++ prjExemploAssociacaoBinaria/src/fatec/poo/model/Departamento.java
@@ -1,10 +1,12 @@
-
+import java.util.ArrayList;
+import java.util.List;
 package fatec.poo.model;
 
 public class Departamento {
     private String sigla, nome;
     private int numFunc;
-    private Funcionario funcionarios[]; 
+    //private Funcionario funcionarios[]; 
+	private List<Funcionario> funcionarios = new ArrayList<Funcionario>(); 
 
     public Departamento(String sigla, String nome) {
         this.sigla = sigla;
@@ -17,6 +19,16 @@ public class Departamento {
 
     public String getNome() {
         return nome;
+    }
+	
+	public void addFuncionario(Funcionario f){
+        funcionarios.add(f);
+    }
+    
+    public void listaFuncionarios(){
+        for(Funcionario funcionario: funcionarios ){
+            System.out.println(funcionario.getNome());
+        }
     }
     
     
