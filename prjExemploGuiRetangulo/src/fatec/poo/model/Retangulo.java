12914fb96cd65f1908041883a5a14957d104af49
Pasquale <pasqualenetto@yahoo.com.br>
2025-05-05 22:35:36 -0300

    add guiRET

---
+++ prjExemploGuiRetangulo/src/fatec/poo/model/Retangulo.java
@@ -0,0 +1,36 @@
+package fatec.poo.model;
+
+/**
+ *
+ * @author Dimas
+ */
+public class Retangulo {
+    private double altura;
+    private double base;
+    
+    public void setAltura(double a){
+        altura = a;
+    }
+    public double getAltura(){
+        return(altura);
+    }
+    
+    public void setBase(double b){
+        base = b;
+    }
+    public double getBase(){
+        return(base);
+    }
+    
+    public double calcArea(){
+        return(base * altura);
+    }
+    
+    public double calcPerimetro(){
+        return(2*(altura + base));
+    }
+    
+    public double calcDiagonal(){
+        return(Math.sqrt(Math.pow(altura, 2)+ Math.pow(base, 2)));
+    }
+}
