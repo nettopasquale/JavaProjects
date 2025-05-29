d318cf4b5180cb2cc6251b181ee2dca2cf3c5105
Pasquale <pasqualenetto@yahoo.com.br>
2025-05-28 21:04:10 -0300

    Add lógica DaoRecepcionista

--- prjPOOAdrianoPasquale/src/fatec/poo/control/DaoRecepcionista.java
+++ prjPOOAdrianoPasquale/src/fatec/poo/control/DaoRecepcionista.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import fatec.poo.model.Recepcionista;
-import java.util.ArrayList;
 
 /**
  *
@@ -22,28 +21,87 @@ public class DaoRecepcionista {
     }
     
     public void inserir(Recepcionista recepcionista){
-        //
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("INSERT INTO "
+                    + "tblRecepcionista(RegFunc_Recep, "
+                    + "Nome_Recep, "
+                    + "Endereco_Recep, "
+                    + "Telefone_Recep, "
+                    + "Turno_Recep) "
+                    + "VALUES(?,?,?,?,?)");
+            
+            ps.setInt(1, recepcionista.getRegFunc());
+            ps.setString(2, recepcionista.getNome());
+            ps.setString(3, recepcionista.getEndereco());
+            ps.setString(4, recepcionista.getTelefone());
+            ps.setString(5, recepcionista.getTurno());
+                      
+            ps.execute(); //envia a instrução SQL para o SGBD
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
     }
     
-    public void alterar(Recepcionista Recepcionista){
-        //
+    public void alterar(Recepcionista recepcionista){
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("Update tblRecepcionista"
+                    + "Nome_Recep = ?, "
+                    + "Endereco_Recep = ?, "
+                    + "Telefone_Recep = ?, "
+                    + "Turno_Recep = ?, "
+                    + "WHERE RegFunc_Recep = ?");
+            
+            ps.setString(1, recepcionista.getNome());
+            ps.setString(2, recepcionista.getEndereco());
+            ps.setString(3, recepcionista.getTelefone());
+            ps.setString(4, recepcionista.getTurno());
+            ps.setInt(5, recepcionista.getRegFunc());
+                      
+            ps.execute(); //envia a instrução SQL para o SGBD
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
     }
     
     public Recepcionista consultar(int regFun){
         Recepcionista r = null;
+        PreparedStatement ps;
+        
+        try {
+            ps = conn.prepareStatement("SELECT * from tblRecepcionista"
+                    + " where RegFunc_Recep = ?");
+            
+            ps.setInt(1, regFun);
+            ResultSet rs = ps.executeQuery();
+           
+            if (rs.next() == true) {
+                r = new Recepcionista(rs.getString("Nome_Recep"),
+                        rs.getInt("RegFunc_Recep"));
+
+            }
+        }
+        catch (SQLException ex) { 
+             System.out.println(ex.toString());   
+        }
         
         return r;
     }
     
-    public void excluir (Recepcionista hospede){
-    
+    public void excluir (Recepcionista recepcionista){
+        PreparedStatement ps;
+        try {
+            ps = conn.prepareStatement("DELETE FROM tblRecepcionista "
+                    + "where RegFunc_Recep = ?");
+            
+            ps.setInt(1, recepcionista.getRegFunc());
+                      
+            ps.execute(); //Envia a instrução SQL para o SGBD
+        } catch (SQLException ex) {
+             System.out.println(ex.toString());   
+        }
     }
     
-    public ArrayList<Recepcionista> consultarServicosQuarto(){
-        ArrayList<Recepcionista> recepcionistas = new ArrayList<>();
-        
-        
-        return recepcionistas;
-    }
     
 }
